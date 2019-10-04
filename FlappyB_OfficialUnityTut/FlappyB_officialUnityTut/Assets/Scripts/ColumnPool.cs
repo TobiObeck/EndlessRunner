e94abf26ab62d4e3875c319c0ed0b6729766f063
Tobi <tobiasoberbeck@gmx.de>
2019-10-04 13:03:31 +0200

    adds flappy bird tutorial follow along code

---
+++ FlappyB_OfficialUnityTut/FlappyB_officialUnityTut/Assets/Scripts/ColumnPool.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ColumnPool : MonoBehaviour {
+
+    public int columnPoolSize = 5;
+    public GameObject columnPrefab;
+    public float spawnRate = 4f;
+    public float columnMin = -5f;
+    public float columnMax = -1.5f;
+
+    private GameObject[] columns;
+    private Vector2 objectPoolPos = new Vector2(-15, 0);
+    private float timeSinceLastSpawned = 2f;
+    private float spawnXPosition = 10f;
+    private int currentColumn = 0;
+
+	// Use this for initialization
+	void Start ()
+    {
+        columns = new GameObject[columnPoolSize];
+        for(int i = 0; i < columnPoolSize; i++)
+        {
+            columns[i] = (GameObject)Instantiate(
+                                    this.columnPrefab
+                                    , this.objectPoolPos
+                                    , Quaternion.identity);
+        }
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        this.timeSinceLastSpawned += Time.deltaTime;
+
+        if(GameControl.instance.gameOver == false
+                    && this.timeSinceLastSpawned >= this.spawnRate)
+        {
+            if (currentColumn >= columnPoolSize)
+            {
+                currentColumn = 0;
+            }
+            this.timeSinceLastSpawned = 0;
+            float spawnYPosition = Random.Range(this.columnMin, this.columnMax);
+            this.columns[this.currentColumn].transform.position = new Vector2(this.spawnXPosition, spawnYPosition);
+            currentColumn++;
+        }
+	}
+}
---
+++ FlappyB_OfficialUnityTut/FlappyB_officialUnityTut/Assets/Scripts/GameControl.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class GameControl : MonoBehaviour {
+
+    public static GameControl instance;
+    public GameObject gameOverText;
+    public Text scoreText;
+    public bool gameOver = false;
+
+    public float scrollSpeed = -1.5f;
+
+    private int score = 0;
+
+	// Use this for initialization
+	void Awake ()
+    {
+        if (!instance)
+        {
+            instance = this;
+        }
+        else if(instance != this)
+        {
+            Destroy(gameObject);
+        }
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+		if( this.gameOver == true && Input.GetMouseButtonDown(0))
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+        }
+	}
+
+    public void BirdScored()
+    {
+        if(this.gameOver == true)
+        {
+            return;
+        }
+        else
+        {
+            this.score++;
+            this.scoreText.text = "Score: " + this.score.ToString();
+        }
+    }
+
+    public void BirdDied()
+    {
+        this.gameOverText.SetActive(true);
+        this.gameOver = true;
+    }
+}
